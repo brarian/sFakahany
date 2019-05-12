8b1ce19728932381bbf2128732077a726752f582
Brarian <b.namonywa@gmail.com>
2019-05-12 10:29:50 -0500

    favicons added

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 function openNav() {
   document.getElementById("mySidenav").style.width = "30vw";
   document.getElementsByClassName("container")[0].style.marginLeft="70px";
@@ -10,3 +8,4 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementsByClassName("container")[0].style.marginLeft="0";
 }
+
