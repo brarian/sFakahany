4f1d723acf03a2eb240b46c59e9e88109f00e3fb
Brarian <b.namonywa@gmail.com>
2019-05-12 10:29:26 -0500

    nodemailer issues

--- server/server.js
+++ server/server.js
@@ -1,38 +1,39 @@
-const bodyParser = require("body-parser");
 const credentials = require("./keys");
 const express = require("express");
 const nodemailer = require("nodemailer");
 
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
 
 app.get("/", (req, res) => res.send("hello"));
 
-app.get("/contact", (req, res) => res.send("contact page"));
+// app.get("/contact", (req, res) => res.render("contact.html"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.post("/contact", function(req, res) {
-  let transporter = nodemailer.createTransport({
-    service: "Gmail",
+app.post("/send-email", function(req, res) {
+  let transporter = nodemailer.createTransport("SMTP", {
+    name: 'iCloud',
     auth: {
       user: credentials.mail,
       pass: credentials.key
     }
   });
-
+  
   let mailOptions = {
-    from: req.body.email, 
-    to: 'b.namonywa@gmail.com', 
-    subject: req.body.subject, 
-    message: req.body.message
-  }; 
-
-  transporter.sendMail(mailOptions, function(error, sucess){
-    if (error){
-      console.log(error)
+    from: req.body.email,
+    to: credentials.mail,
+    subject: req.body.subject,
+    text: req.body.message
+  };
+  console.log(req);
+  
+  transporter.sendMail(mailOptions, (error, info)=> {
+    if (error) {
+      console.log(error);
     } else {
-      console.log('sent')
+      console.log('Email sent: ' + info.response);
     }
-  })
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
